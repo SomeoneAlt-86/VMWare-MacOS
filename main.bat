7313a85b219168ed32dbb37d7baf1064526577e0
SomeoneAlt-86 <76504556+SomeoneAlt-86@users.noreply.github.com>
2023-07-14 08:56:58 +0530

    Create main.bat

---
+++ main.bat
@@ -0,0 +1,29 @@
+echo Installing requirements for installation 
+
+wget https://qemu.weilnetz.de/w64/qemu-w64-setup-20230629.exe
+qemu-w64-setup-20230629.exe
+wget https://raw.githubusercontent.com/acidanthera/OpenCorePkg/master/Utilities/macrecovery/macrecovery.py
+
+echo Done!
+
+echo Downloading MacOS
+python3 macrecovery.py -b Mac-FFE5EF870D7BA81A -m 00000000000000000 download
+
+echo Preparing drive 
+"C:\Program Files\qemu\qemu-img.exe" convert -O vmdk -o compat6 com.apple.recovery.boot\BaseSystem.dmg recovery.vmdk
+
+echo Patching VMWare [Please close VMWare Workstation or Player]
+timeout /5
+
+wget https://raw.githubusercontent.com/paolo-projects/unlocker/master/win-install.cmd
+win-install.cmd 
+
+echo Done!
+
+echo All set, launch macOS 10.15.vmx from the folder and add Recovery.vmdk along with installtion vmdk.
+
+
+
+
+
+
